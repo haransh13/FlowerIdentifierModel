5c1df3f0b060d1df759fd2b2c8bbb540df65a46f
haransh13 <135721876+haransh13@users.noreply.github.com>
2024-06-24 23:32:04 +0530

    project1.py

--- project1.py
+++ project1.py
@@ -1,21 +1,3 @@
-import sys
-print('Python: {}'.format(sys.version))
-# scipy
-import scipy
-print('scipy: {}'.format(scipy.__version__))
-# numpy
-import numpy
-print('numpy: {}'.format(numpy.__version__))
-# matplotlib
-import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
-# pandas
-import pandas
-print('pandas: {}'.format(pandas.__version__))
-# scikit-learn
-import sklearn
-print('sklearn: {}'.format(sklearn.__version__))
-# Load libraries
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
@@ -33,32 +15,20 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
 
-# summarize the data
+
 from pandas import read_csv
-# Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
-# shape
 print(dataset.shape)
-# head
 print(dataset.head(20))
-# descriptions
 print(dataset.describe())
-# class distribution
 print(dataset.groupby('class').size())
 
-
-# visualize the data
-from pandas import read_csv
-from pandas.plotting import scatter_matrix
-from matplotlib import pyplot as plt
-
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
-# Split-out validation dataset
 array = dataset.values
 X = array[:,0:4]
 y = array[:,4]
@@ -67,8 +37,7 @@ models = []
 model =  LogisticRegression(solver='liblinear', multi_class='ovr')
 model.fit(X_train, Y_train)
 predictions = model. predict (X_validation)
-
-#evaluate predictions 
+ 
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation,predictions))
 print(classification_report(Y_validation,predictions))
